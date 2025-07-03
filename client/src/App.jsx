eb4146690f3630bf9e2cc84a56e84a9caf4c915b
adelynntran <161761042+adelynntran@users.noreply.github.com>
2025-07-03 18:35:52 -0400

    update App.jsx, wrap it in AppProvider and use HomePage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 //Main App component (wrap with AppProvider)
 import React from 'react';
 import {AppProvider} from './context/AppContext';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
 //wrap main App component with <AppProvider> to make
 //the context available to all child components
 <AppProvider>
-
+<div className="App">
+  <HomePage />
+</div>
 </AppProvider>
   );
 }
