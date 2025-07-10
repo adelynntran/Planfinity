d28bd67ebe1ff28d98c73a9d031c00d4794dc3ae
adelynntran <161761042+adelynntran@users.noreply.github.com>
2025-07-10 16:59:56 -0400

    update with TermCard changes, add total credits

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,11 +6,14 @@ import TermCard from '../components/term/TermCard';
 function HomePage() {
 
     //add all context value that you want to use here:
-    const { state, calculateCumulativeGPA } = useApp();
+    const { state, 
+        calculateCumulativeGPA,
+    getDegreeCredits } = useApp();
     
     //declare variables here:
     const currentCGPA = calculateCumulativeGPA();
     const terms = Object.keys(state.currentPlan.terms);
+    const totalCredits = getDegreeCredits();
     
     return (
         <div style={{ padding: '20px', maxWidth: '1200px', margin: '0 auto' }}>
@@ -20,6 +23,7 @@ function HomePage() {
           <div style={{ margin: '20px 0', padding: '15px', backgroundColor: '#f8fafc', borderRadius: '8px' }}>
             <p><strong style={{ color: '#111827', fontSize: '14px' }}>Total Courses:</strong> <strong style={{ color: '#111827', fontSize: '14px' }}></strong><strong style={{ color: '#111827', fontSize: '14px' }}>{state.courses.length}</strong></p>
             <p><strong style={{ color: '#111827', fontSize: '14px' }}>Current CGPA:</strong> <strong style={{ color: '#111827', fontSize: '14px' }}>{currentCGPA.gpa}</strong></p>
+            <p><strong style={{ color: '#111827', fontSize: '14px' }}>Total Credits:</strong> <strong style={{ color: '#111827', fontSize: '14px' }}>{totalCredits}</strong></p>
           </div>
       
           <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))', gap: '15px' }}>
