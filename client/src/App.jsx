3b0f4425793136921ba434bbadb7a4a11cdb46e4
adelynntran <161761042+adelynntran@users.noreply.github.com>
2025-07-12 16:51:48 -0400

    update router, routes and route for navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,30 @@
 //Main App component (wrap with AppProvider)
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {AppProvider} from './context/AppContext';
+
+//import pages here:
 import HomePage from './pages/HomePage';
+import IndividualCoursePage from './pages/IndividualCoursePage';
 
 function App() {
   return (
 //wrap main App component with <AppProvider> to make
 //the context available to all child components
 <AppProvider>
-<div className="App">
-  <HomePage />
-</div>
+
+  {/*Add <Router> to use useNavigate() */}
+  <Router>
+  <div className="App">
+
+    {/* add routes to pages here*/}
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/course/:courseId/" element={<IndividualCoursePage />} />
+    </Routes>
+  
+  </div>
+  </Router>
 </AppProvider>
   );
 }
