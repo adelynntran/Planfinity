03a710db6eb86912bbc25c35647633b0160c236d
adelynntran <161761042+adelynntran@users.noreply.github.com>
2025-07-14 19:16:15 -0400

    remove cgpa from body

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -20,7 +20,7 @@ function HomePage() {
           
           <div style={{ margin: '20px 0', padding: '15px', backgroundColor: '#f8fafc', borderRadius: '8px' }}>
             <p><strong style={{ color: '#111827', fontSize: '14px' }}>Total Courses:</strong> <strong style={{ color: '#111827', fontSize: '14px' }}></strong><strong style={{ color: '#111827', fontSize: '14px' }}>{state.courses.length}</strong></p>
-            <p><strong style={{ color: '#111827', fontSize: '14px' }}>Current CGPA:</strong> <strong style={{ color: '#111827', fontSize: '14px' }}>{currentCGPA.gpa}</strong></p>
+            {/*<p><strong style={{ color: '#111827', fontSize: '14px' }}>Current CGPA:</strong> <strong style={{ color: '#111827', fontSize: '14px' }}>{currentCGPA.gpa}</strong></p>*/}
             <p><strong style={{ color: '#111827', fontSize: '14px' }}>Total Credits:</strong> <strong style={{ color: '#111827', fontSize: '14px' }}>{totalCredits}</strong></p>
           </div>
       
