48af0775597ec1f56d51295f860d0872704e6ba7
adelynntran <161761042+adelynntran@users.noreply.github.com>
2025-07-30 20:19:53 -0400

    add necessary files for backend database

---
+++ client/.vite/deps/chunk-74JV4TBK.js
@@ -0,0 +1,11 @@
+import {
+  capitalize
+} from "./chunk-GLD7Q6RD.js";
+
+// node_modules/@mui/material/esm/utils/capitalize.js
+var capitalize_default = capitalize;
+
+export {
+  capitalize_default
+};
+//# sourceMappingURL=chunk-74JV4TBK.js.map
